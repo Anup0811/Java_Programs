9f91a7cb38b5fda0764e4c2fd6144a59b4cd3d86
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 22:50:59 +0530

    Update Multiplication.java

--- Multiplication/Multiplication.java
+++ Multiplication/Multiplication.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        
+        int num1,num2,mult;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first Number :");
+        num1=sc.nextInt();
+
+        System.out.println("Enter second Number :");
+        num2=sc.nextInt();
+
+        mult=num1*num2;
+        System.out.println("Multiplication is :"+mult);
+
+    }
+    
+}
 
