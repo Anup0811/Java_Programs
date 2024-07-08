1460700cec37267eaf6d7870d89534225c8be1c6
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 23:11:04 +0530

    Update PositiveNegative.java

--- PositiveNegativeNo/PositiveNegative.java
+++ PositiveNegativeNo/PositiveNegative.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        
+        int num;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a Number :");
+        num=sc.nextInt();
+
+        if(num>0)
+        {
+            System.out.println("Number is Positive");
+        }
+        else if(num<0)
+        {
+            System.out.println("Number is Negative");
+        }
+        else
+        {
+            System.out.println("Number is equal to Zero");
+        }
+    }
+}
 
