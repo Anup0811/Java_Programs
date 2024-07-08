936ce06313020e35750c86c20aaceed761f5ee7d
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 23:01:29 +0530

    Update EvenOdd.java

--- EvenOdd/EvenOdd.java
+++ EvenOdd/EvenOdd.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        
+        int num;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a Number :");
+        num=sc.nextInt();
+
+        if(num%2==0)
+        {
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is Odd");
+        }
+    }
+}
 
