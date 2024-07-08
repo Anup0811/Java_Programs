c3bc64955825ce2ebd45462f14d1a3bef7ac2449
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 22:53:04 +0530

    Update Division.java

--- Division/Division.java
+++ Division/Division.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        
+        int num1,num2,div;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter first Number :");
+        num1=sc.nextInt();
+
+        System.out.println("Enter second Number :");
+        num2=sc.nextInt();
+
+        div=num1/num2;
+        System.out.println("Division is :"+div);
+
+    }
+}
 
