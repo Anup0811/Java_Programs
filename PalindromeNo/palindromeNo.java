17e6171b7685d17b1d70118b01bf8a98ef34923b
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 23:28:27 +0530

    Update palindromeNo.java

--- PalindromeNo/palindromeNo.java
+++ PalindromeNo/palindromeNo.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class PalindromeNo {
+    public static void main(String[] args) {
+        
+        int num,rem,temp,sum=0;
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        num=sc.nextInt();
+        temp=num;
+        while(num>0)
+        {
+            rem=num%10;
+            sum=(sum*10)+rem;
+            num=num/10;
+        }
+        if(temp==sum)
+        {
+            System.out.println("Number is Palindrome");
+        }
+        else
+        {
+            System.out.println("Number is not Palindrome");
+        }
+    }
+}
 
