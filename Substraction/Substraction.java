dffcdf123994405e4640c1829983037b7d4dedf4
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-08 22:47:34 +0530

    Update Substraction.java

--- Substraction/Substraction.java
+++ Substraction/Substraction.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Substraction {
+    public static void main(String[] args) {
+        int a,b,sub;
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter first Number :");
+        a=sc.nextInt();
+
+        System.out.println("Enter Second Number :");
+        b=sc.nextInt();
+
+        sub=a-b;
+        System.out.println("Substraction is:"+sub);
+        sc.close();
+
+    }
+}
 
