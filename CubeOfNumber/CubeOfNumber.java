66c69c834b4ea663ba58fc43eccb0145557f339b
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-15 23:28:02 +0530

    Update CubeOfNumber.java

--- CubeOfNumber/CubeOfNumber.java
+++ CubeOfNumber/CubeOfNumber.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class CubeOfNo {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number :");
+        int num = sc.nextInt();
+        int cube;
+
+        cube = num * num * num;
+
+        System.out.println("Cube of "+num+" is "+cube);
+    }
+    
+}
 
