83c3956ee5c37eddefb36a4957d464701e9ead3b
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-07-15 23:24:11 +0530

    Update SquareOfNo.java

--- SquareOfNumber/SquareOfNo.java
+++ SquareOfNumber/SquareOfNo.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class SquareOfNo {
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number :");
+        int num= sc.nextInt();
+        int sq;
+
+        sq= num * num;
+        System.out.println("Square of "+num+" is "+sq);
+
+    }
+    
+}
 
